411e8a3770b14b714e6b8e52503cb8e6a85ffe75
Timm Knape <timm@knp.de>
2024-02-04 23:32:08 +0100

    more test cases

--- tests/test-add.cpp
+++ tests/test-add.cpp
@@ -22,3 +22,25 @@ TEST(add_tests, negative) {
 	signed char expected[] { -1, -1, -1, -10 };
 	EXPECT_STACK(code, expected);
 }
+
+TEST(add_tests, big_ok_a) {
+	signed char code[] {
+		vm::op_push_ch, -1, vm::op_push_ch, -1,
+		vm::op_push_ch, -1, vm::op_push_ch, 127,
+		vm::op_push_ch, 0, vm::op_ch_to_int,
+		vm::op_add_int, vm::op_break
+	};
+	signed char expected[] { 127, -1, -1, -1 };
+	EXPECT_STACK(code, expected);
+}
+
+TEST(add_tests, big_ok_b) {
+	signed char code[] {
+		vm::op_push_ch, 0, vm::op_ch_to_int,
+		vm::op_push_ch, -1, vm::op_push_ch, -1,
+		vm::op_push_ch, -1, vm::op_push_ch, 127,
+		vm::op_add_int, vm::op_break
+	};
+	signed char expected[] { 127, -1, -1, -1 };
+	EXPECT_STACK(code, expected);
+}
