35aa9486f7592011391d123779e665f988093333
Timm Knape <timm@knp.de>
2024-02-24 14:59:21 +0100

    test that stack is cleared on return

--- tests/test-call.cpp
+++ tests/test-call.cpp
@@ -14,3 +14,14 @@ TEST(call_tests, simple) {
 	signed char expected[] { };
 	EXPECT_LIMITED_STACK(code, stack_frame_size, expected);
 }
+
+TEST(call_tests, return_with_noempty_stack) {
+	signed char code[] {
+		op_near_jmp, 3,
+		PUSH_CH(10), op_return,
+		PUSH_CH(2), op_call,
+		PUSH_CH(2), op_call
+	};
+	signed char expected[] { };
+	EXPECT_LIMITED_STACK(code, stack_frame_size + ch_size, expected);
+}
