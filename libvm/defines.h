017fa33348a3faa3804450d2ca0dfc3275f0244c
Timm Knape <timm.knape@mechatronic.de>
2024-02-26 17:16:34 +0100

    working on more flexible configuration

---
+++ libvm/defines.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#if !defined(CONFIG_WITH_INT)
+	#define CONFIG_WITH_INT true
+#endif
+
+#if CONFIG_WITH_INT
+	#if !defined(CONFIG_INT_SIZE)
+		#define CONFIG_INT_SIZE 3
+	#endif
+	#if !defined(CONFIG_INT_TYPE)
+		#define CONFIG_INT_TYPE int
+	#endif
+	#if !defined(CONFIG_INT_USE_TYPE_FIELD)
+		#define CONFIG_INT_USE_TYPE_FIELD true
+	#endif
+	#if !defined(CONFIG_INT_MULTI_PRECISION)
+		#define CONFIG_INT_MULTI_PRECISION false
+	#endif
+#endif
+
+#if !defined(CONFIG_WITH_CHAR)
+	#define CONFIG_WITH_CHAR true
+#endif
+
+#if CONFIG_WITH_CHAR
+	#if !defined(CONFIG_CHAR_SIZE)
+		#define CONFIG_CHAR_SIZE 1
+	#endif
+	#if !defined(CONFIG_CHAR_TYPE)
+		#define CONFIG_CHAR_TYPE signed char
+	#endif
+	#if !defined(CONFIG_CHAR_USE_TYPE_FIELD)
+		#define CONFIG_CHAR_USE_TYPE_FIELD false
+	#endif
+#endif
+
+#if !defined(CONFIG_WITH_CALL)
+	#define CONFIG_WITH_CALL true
+#endif
+
+#if CONFIG_WITH_CALL
+	#if !CONFIG_WITH_INT
+		#error "call needs int"
+	#endif
+#endif
+
+#if !defined(CONFIG_WITH_HEAP)
+	#define CONFIG_WITH_HEAP true
+#endif
+
+#if CONFIG_WITH_HEAP
+	#if !CONFIG_WITH_INT
+		#error "heap needs int"
+	#endif
+
+	#if !defined(CONFIG_HEAP_PTR_SIZE)
+		#define CONFIG_HEAP_PTR_SIZE CONFIG_INT_SIZE
+	#endif
+	#if !defined(CONFIG_HEAP_PTR_USE_TYPE_FIELD)
+		#define CONFIG_HEAP_PTR_USE_TYPE_FIELD CONFIG_INT_USE_TYPE_FIELD
+	#endif
+	#if !defined(CONFIG_WITH_GC)
+		#define CONFIG_WITH_GC true
+	#endif
+#endif
+
+#if !defined(CONFIG_STACK_PTR_SIZE)
+	#define CONFIG_STACK_PTR_SIZE CONFIG_INT_SIZE
+#endif
+#if !defined(CONFIG_STACK_PTR_USE_TYPE_FIELD)
+	#define CONFIG_STACK_PTR_USE_TYPE_FIELD CONFIG_INT_USE_TYPE_FIELD
+#endif
--- libvm/value.h
+++ libvm/value.h
@@ -2,19 +2,97 @@
 
 #include <variant>
 
+#include "defines.h"
+
 #include "ptr.h"
 
 namespace vm {
-	struct Stack_Frame {
-		Code_Ptr pc;
-		Ram_Ptr parent;
-		Ram_Ptr outer;
-	};
+	#if CONFIG_WITH_INT
+		struct Int {
+			using value_type = CONFIG_INT_TYPE;
+			static constexpr int raw_size { CONFIG_INT_SIZE };
+			static constexpr int typed_size { raw_size + 1 };
+			static constexpr bool use_type_field { CONFIG_INT_USE_TYPE_FIELD };
+			static constexpr bool is_multi_precision {
+				CONFIG_INT_MULTI_PRECISION
+			};
+			value_type value;
+		};
 
-	bool operator==(const Stack_Frame& a, const Stack_Frame& b);
-	bool operator<(const Stack_Frame& a, const Stack_Frame& b);
+		#if CONFIG_INT_MULTI_PRECISION
+			bool operator==(const Int& a, const Int& b);
+			bool operator<(const Int& a, const Int& b);
+		#else
+			inline bool operator==(const Int& a, const Int& b) {
+				return a.value == b.value;
+			}
 
-	using Value = std::variant<signed char, int, Heap_Ptr, Stack_Frame>;
+			inline bool operator<(const Int& a, const Int& b) {
+				return a.value < b.value;
+			}
+		#endif
+	#endif
+
+	#if CONFIG_WITH_CHAR
+		struct Char {
+			using value_type = CONFIG_CHAR_TYPE;
+			static constexpr int raw_size { CONFIG_CHAR_SIZE };
+			static constexpr int typed_size { raw_size + 1 };
+			static constexpr bool use_type_field { CONFIG_CHAR_USE_TYPE_FIELD };
+			value_type value;
+		};
+
+		inline bool operator==(const Char& a, const Char& b) {
+			return a.value == b.value;
+		}
+
+		inline bool operator<(const Char& a, const Char& b) {
+			return a.value < b.value;
+		}
+
+	#endif
+
+	#if CONFIG_WITH_CALL
+		struct Stack_Frame {
+			Code_Ptr pc;
+			Ram_Ptr parent;
+			Ram_Ptr outer;
+		};
+
+		bool operator==(const Stack_Frame& a, const Stack_Frame& b);
+		bool operator<(const Stack_Frame& a, const Stack_Frame& b);
+	#endif
+
+	using Value = std::variant<
+		#if CONFIG_WITH_CHAR
+			#define VARIANT_SEP 1
+			signed char
+		#endif
+		#if CONFIG_WITH_INT
+			#if !defined(VARIANT_SEP)
+				#define VARIANT_SEP 1
+			#else
+				,
+			#endif
+			int
+		#endif
+		#if CONFIG_WITH_HEAP
+			#if !defined(VARIANT_SEP)
+				#define VARIANT_SEP 1
+			#else
+				,
+			#endif
+			Heap_Ptr
+		#endif
+		#if CONFIG_WITH_CALL
+			#if !defined(VARIANT_SEP)
+				#define VARIANT_SEP 1
+			#else
+				,
+			#endif
+			Stack_Frame
+		#endif
+	>;
 
 	int value_size(signed char type);
 	int value_size(const Value& value);
