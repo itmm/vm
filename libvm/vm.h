40c0b6eafa16c8789c30e043f63eba234bb2f1c3
Timm Knape <timm.knape@mechatronic.de>
2024-02-28 12:53:47 +0100

    moved Char and Int to value.h

--- libvm/vm.h
+++ libvm/vm.h
@@ -1,54 +1,9 @@
 #pragma once
 
 #include "defines.h"
+#include "value.h"
 
 namespace vm {
-	#if CONFIG_WITH_INT
-		struct Int {
-			using value_type = CONFIG_INT_TYPE;
-			static constexpr signed char type_ch { 0x20 };
-			static constexpr int raw_size { CONFIG_INT_SIZE };
-			static constexpr int typed_size { raw_size + 1 };
-			static constexpr bool use_type_field { CONFIG_INT_USE_TYPE_FIELD };
-			static constexpr bool is_multi_precision {
-				CONFIG_INT_MULTI_PRECISION
-			};
-			value_type value;
-		};
-
-		#if CONFIG_INT_MULTI_PRECISION
-			bool operator==(const Int& a, const Int& b);
-			bool operator<(const Int& a, const Int& b);
-		#else
-			inline bool operator==(const Int& a, const Int& b) {
-				return a.value == b.value;
-			}
-
-			inline bool operator<(const Int& a, const Int& b) {
-				return a.value < b.value;
-			}
-		#endif
-	#endif
-
-	#if CONFIG_WITH_CHAR
-		struct Char {
-			using value_type = CONFIG_CHAR_TYPE;
-			static constexpr signed char type_ch { 0x10 };
-			static constexpr int raw_size { CONFIG_CHAR_SIZE };
-			static constexpr int typed_size { raw_size + 1 };
-			static constexpr bool use_type_field { CONFIG_CHAR_USE_TYPE_FIELD };
-			value_type value;
-		};
-
-		inline bool operator==(const Char& a, const Char& b) {
-			return a.value == b.value;
-		}
-
-		inline bool operator<(const Char& a, const Char& b) {
-			return a.value < b.value;
-		}
-	#endif
-
 	constexpr signed char ptr_type { 0x30 };
 	constexpr signed char stack_frame_type { 0x40 };
 
